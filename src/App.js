169993119186b80225b66d07604f051241f09ffa
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-23 13:40:08 -0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,18 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import Routes from './routes';
+import { AuthProvider } from './contexts/AuthContext';
 
+const Stack = createNativeStackNavigator();
+
+export default function App() {
+
+  return (
+    <NavigationContainer>
+        <AuthProvider>
+          <Routes/>
+        </AuthProvider>
+    </NavigationContainer>
+  );
+}
