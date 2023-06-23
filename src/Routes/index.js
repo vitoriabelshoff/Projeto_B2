aa93c327b08e0cf868a9c720ac02338b472740ce
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-23 13:48:54 -0300

    Update index.js

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -1,1 +1,19 @@
+import AuthRoutes from "./AuthRoutes";
+import AppRoutes from "./AppRoutes";
+import { useAuth } from "../contexts/AuthContext";
+import { ActivityIndicator, View } from "react-native";
 
+
+export default function Routes() {
+    const { logado, loading } = useAuth();
+  
+    if (loading) {
+      return (
+        <View>
+          <ActivityIndicator size="large" />
+        </View>
+      );
+    }
+  
+    return logado ? <AppRoutes /> : <AuthRoutes />;
+  }
