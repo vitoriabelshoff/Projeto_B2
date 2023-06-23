aa1fdc770ca89814472bb9bfdbd839fabc8126ce
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-23 13:48:23 -0300

    Update AppRoutes.js

--- src/Routes/AppRoutes.js
+++ src/Routes/AppRoutes.js
@@ -1,1 +1,15 @@
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import MoviesPage from '../pages/MoviesPage';
+import MovieDetailsPage from '../pages/MovieDetailsPage';
 
+const Stack = createNativeStackNavigator();
+
+
+export default function AppRoutes(){
+    return(
+        <Stack.Navigator>
+        <Stack.Screen name='MoviesPage' component={MoviesPage} options={{headerShown: false}}/>
+        <Stack.Screen name='MovieDetailsPage' component={MovieDetailsPage}/>
+        </Stack.Navigator>
+    );
+}
