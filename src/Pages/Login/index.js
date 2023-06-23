4bce3cf3102c295c00602f8e95574aaa1c750d27
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-23 14:04:54 -0300

    Update index.js

--- src/Pages/Login/index.js
+++ src/Pages/Login/index.js
@@ -1,1 +1,24 @@
+import { TouchableOpacity, View, TextInput, Image, Text } from "react-native"
+import { Link } from "@react-navigation/native"
+import styles from "./styles"
+import { useContext } from "react";
+import AuthContext from "../../contexts/AuthContext";
 
+export default function LoginPage(){
+  const { SignIn } = useContext(AuthContext);
+
+  return(
+    <View style={styles.container}>
+      <Image source={require('/src/LOGO.png')} style = {styles.imgLogo}/>
+      <TextInput placeholder="Email" style={styles.input}/>
+      <TextInput placeholder="Senha"style={styles.input}/>
+      <TouchableOpacity style={styles.btnLogin} onPress={() => SignIn()}>
+        <Text>Login</Text>
+      </TouchableOpacity>
+      <View style={styles.viewLinks}>
+        <Link to={{screen: "RegisterPage"}}>Registre-se</Link>
+        <Link to={{screen: "ForgetPasswordPage"}}>Esqueceu a senha?</Link>
+      </View>
+    </View>
+  )
+};
