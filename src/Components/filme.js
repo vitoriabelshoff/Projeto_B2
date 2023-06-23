3e75214c92b2a1bc76595d20f512ea67993ada85
Vitoria Belshoff <103432976+vitoriabelshoff@users.noreply.github.com>
2023-06-23 14:11:23 -0300

    Update filme.js

--- src/Components/filme.js
+++ src/Components/filme.js
@@ -1,1 +1,22 @@
+import { Image, View, Text} from "react-native";
+import estilo from "./styles";
+import { TouchableOpacity } from 'react-native';
 
+
+export default function Filme({ data }) {
+
+    const uri = data.attributes.poster.data.attributes.url;
+    const uriConcatenada = "https://api.otaviolube.com" + uri;
+
+    return (
+        <View style={estilo.filmeContainer}>
+            <Text style={estilo.titulo}> {data.attributes.titulo} </Text>
+            <Text style={estilo.subtitulo}> {data.attributes.subtitulo} </Text>
+            <Image source={{ uri: uriConcatenada }} style={estilo.img} />
+            <Text style={estilo.sinopse}> {data.attributes.sinopse.length > 200 ? data.attributes.sinopse.substring(0,200) + "...": sinopse} </Text>
+            <TouchableOpacity onPress={() => navigation.navigate('MovieDetailsPage', { filme: Filme })}>
+            <Text>Saiba mais...</Text>
+            </TouchableOpacity>
+        </View>
+    )
+}
